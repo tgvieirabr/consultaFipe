857c2979dac1c42589ba6b22b4993e08458043e8
tgvieirabr <tgvieirabr@gmail.com>
2022-06-01 16:42:42 -0300

    adc nodeserver data

---
+++ server.js
@@ -0,0 +1,31 @@
+const xl = require('excel4node');
+const wb = new xl.Workbook();
+const ws = wb.addWorksheet('MMV');
+
+const data = [
+
+    {
+        "marca": "MERCEDES",
+        "modelo": "L-13-130",
+        "ano": "1981"
+
+
+
+    },
+    {
+        "marca": "MERCEDES",
+        "modelo": "L-13-1300",
+        "ano": "1982"
+
+
+
+    },
+    {
+        "marca": "MERCEDES",
+        "modelo": "L-13-1300",
+        "ano": "1983"
+
+
+
+    },
+];
